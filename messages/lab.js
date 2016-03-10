001cc8d72a1edb6b8a6ad17646e7e77e66a59296
Alyssa Fusillo <alyssa.fusillo@tufts.edu>
2016-03-10 10:04:27 -0500

    Code is working

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
-// Your JavaScript goes here...
+
+
+
+function parse() {
+	request = new XMLHttpRequest();
+
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange= function() {
+
+	if (request.readyState == 4) {
+		result = "";
+
+		raw = request.responseText;
+
+		parsedObjects = JSON.parse(raw);
+		elem = document.getElementById("messages");
+		for (count = 0; count < parsedObjects.length; count++) {
+			result += "<p>" + parsedObjects[count]["content"] + " " +parsedObjects[count]["username"] + "</p>";
+		}
+			elem.innerHTML = result;
+		}
+};
+
+request.send(null);
+}
